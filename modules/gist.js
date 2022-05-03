2571294024f083a3ade18532fc2adf6c8684799b
codetariat <71725599+codetariat@users.noreply.github.com>
2022-05-03 23:08:30 +0200

    fix gist_id

--- modules/gist.js
+++ modules/gist.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 const GitHub = require('github-base');
 const github = new GitHub({ token: process.env.GITHUB_TOKEN});
-const gist_id = '03043d47689a6ee645366d327b11944c'
+const gist_id = '929307be574de178428d8e3d6710c382'
 
 exports.push = async function(input){
 
