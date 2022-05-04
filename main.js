dd521783528d37ad20188e9ac88f07ae08c14f59
codetariat <71725599+codetariat@users.noreply.github.com>
2022-05-04 20:25:14 +0200

    Elaborated example request

--- main.js
+++ main.js
@@ -1,12 +1,29 @@
 /*
 
-Example request to scrape best price graph from rolimons:
+// Example request to scrape best price and sales volume data from rolimons:
 
 let id = 6803410579
 let res = await fetch(`https://www.rolimons.com/item/${id}`).then(r=>r.text())
-let a = res.substr(res.indexOf(`"best_price":[`) + (`"best_price":`).length)
-let b = JSON.parse(a.substr(0, a.indexOf(`]`)+1))
-console.log(b)
+
+let bestPricesUnprocessed = res.substr(res.indexOf(`"best_price":[`) + (`"best_price":`).length)
+let bestPrices = JSON.parse(bestPricesUnprocessed.substr(0, bestPricesUnprocessed.indexOf(`]`)+1))
+console.log(bestPrices)
+
+let salesDatesUnprocessed = res.substr(res.indexOf(`"timestamp":[`) + (`"timestamp":`).length)
+let salesDates = JSON.parse(salesDatesUnprocessed.substr(0, salesDatesUnprocessed.indexOf(`]`)+1))
+console.log(salesDates)
+
+let salesVolumeUnprocessed = res.substr(res.indexOf(`"sales_volume":[`) + (`"sales_volume":`).length)
+let salesVolume = JSON.parse(salesVolumeUnprocessed.substr(0, salesVolumeUnprocessed.indexOf(`]`)+1))
+console.log(salesVolume)
+
+let salesData = {}
+
+for(let k in salesDates){
+    salesData[salesDates[k]] = salesVolume[k]
+}
+
+console.log(salesData)
 
 */
 
